a8f88c6b0f459169d0ad7bfcde54b52c9759db15
ifly6 <ifly6@me.com>
2020-09-29 18:13:06 -0400

    changes for chart
    added chart generation
    added way to remove aliases from pandas df generation
    added multiple returns for leaderboards creation

--- src/reports/pandas_reports.py
+++ src/reports/pandas_reports.py
@@ -7,18 +7,29 @@ def _count(g):
     return sum(1 for _ in g)
 
 
-def __create_leaderboard_df(db: Database):
-    l = []
+def _flatten(l):
+    return [item for sublist in l for item in sublist]
+
+
+def __create_leaderboard_df(db: Database, keep_puppets=True):
+    alias_df = pd.read_csv('../db/aliases.csv')
+    aliases = set(_flatten(alias_df['Aliases'].str.split(',').values) + list(alias_df['Player'].values))
+    # concat needs list
+
+    rows = []
     for author in (db.authors + db.player_authors):
+        if author.is_player is False and author.name in aliases and keep_puppets is False:
+            continue  # if not keeping puppets, skip non-players who match alias list
+
         d = {'Name': '[PLAYER] ' + author.name if author.is_player else author.name,
              'Authored': len(author.authored_resolutions),
              'Co-authored': len(author.coauthored_resolutions),
              'Repeals': _count(r for r in author.authored_resolutions if r.category in ["Repeal", 'repeal']),
              'Active': _count(r for r in author.authored_resolutions + author.coauthored_resolutions
                               if r.repealed_by is None and r.category not in ['Repeal', 'repeal'])}
-        l.append(d)
+        rows.append(d)
 
-    df = pd.DataFrame(l)
+    df = pd.DataFrame(rows)
 
     # create totals and sort
     df['Total'] = df['Authored'] + df['Co-authored']
@@ -27,8 +38,11 @@ def __create_leaderboard_df(db: Database):
     return df
 
 
-def create_leaderboards(db: Database, format='markdown'):
-    df = __create_leaderboard_df(db)
+def create_leaderboards(db: Database, format='markdown', keep_puppets=True):
+    df = __create_leaderboard_df(db, keep_puppets=keep_puppets)
+
+    if format == 'pandas':
+        return df
 
     if format == 'markdown':
         df['Name'] = df['Name'].str.replace(r'\[PLAYER\]', r'**[PLAYER]**')
